d6267f866f1b23843d662a8bce2c7b7edf1bdc3a
0918nobita <nobita.0918@gmail.com>
2019-04-29 22:04:01 +0900

    :construction: Expr クラスを追加した

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,36 @@ enum ExprType {
   IMPLICATION,
   NEGATION,
 }
+
+class Expr<T extends ExprType> {
+  private params: Expr<any>[];
+
+  constructor(
+    private type: T,
+    ...args:
+        Expr<any>[] &
+        { length: T extends (ExprType.TRUE | ExprType.FALSE) ? 0 : (T extends ExprType.NEGATION ? 1 : 2) }
+  ) {
+    this.params = args;
+  }
+
+  toString(): string {
+    const lhs = () => this.params[0].toString();
+    const rhs = () => this.params[1].toString();
+
+    switch (this.type) {
+      case ExprType.TRUE:
+        return "t";
+      case ExprType.FALSE:
+        return "f";
+      case ExprType.CONJUNCTION:
+        return `* ${lhs()} ${rhs()}`;
+      case ExprType.DISJUNCTION:
+        return `+ ${lhs()} ${rhs()}`;
+      case ExprType.IMPLICATION:
+        return `-> ${lhs()} ${rhs()}`;
+      default:  // NEGATION
+        return `! ${lhs()}`
+    }
+  }
+}
