8d04ea088bb87b6fff3bfbf1be92cf1f86226b9a
0918nobita <nobita.0918@gmail.com>
2019-04-30 19:32:52 +0900

    :sparkles: メソッドに対してデコレータを適用するサンプルを追加した

---
+++ src/decorator.ts
@@ -0,0 +1,24 @@
+function Decorate(option: string) {
+  return (_: any, name: string, descriptor: PropertyDescriptor) => {
+    console.log('Decorate デコレータ付きのメソッドが定義されました');
+
+    const method = descriptor.value;
+
+    descriptor.value = function() {
+      console.log('修飾されたメソッドを呼び出します');
+      const ret = method.apply(this, arguments);
+      console.log('呼び出しが完了しました');
+      return `${option}, ${ret}, ${name}`;
+    };
+  };
+}
+
+class Test {
+  @Decorate('デコレータ')
+  test() {
+    console.log('test');
+    return 42;
+  }
+}
+
+console.log(new Test().test());
