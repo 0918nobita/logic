ad5635fe04a9dd4d406d2713e2674bfbc364a138
0918nobita <nobita.0918@gmail.com>
2019-04-30 15:38:50 +0900

    :sparkles: Expr インターフェース, Variable / LogicExpr クラスを実装した

--- src/main.ts
+++ src/main.ts
@@ -6,42 +6,65 @@ enum ExprType {
   VARIABLE,
 }
 
-class Expr<T extends ExprType> {
-  private params: Expr<any>[];
+interface Expr {
+  type: ExprType;
+  params: Expr[];
+  equals(target: Expr): boolean;
+  toString(): string;
+}
+
+class Variable implements Expr {
+  type = ExprType.VARIABLE;
+  params = [];
+
+  constructor(private name: string) {}
+
+  equals(target: Expr): boolean {
+    if (target.type !== ExprType.VARIABLE) return false;
+    return (target as Variable).name === this.name;
+  }
+
+  toString(): string {
+    return this.name;
+  }
+}
+
+class LogicExpr<T extends ExprType> implements Expr {
+  params: Expr[];
 
   constructor(
     public type: T,
     ...args:
-        Expr<any>[] &
+        Expr[] &
         { length: T extends (ExprType.TRUE | ExprType.FALSE) ? 0 : (T extends ExprType.NEGATION ? 1 : 2) }
   ) {
     this.params = args;
   }
 
-  equals(expr: Expr<any>): boolean {
-    if (expr.type !== this.type) return false;
+  equals(target: Expr): boolean {
+    if (target.type !== this.type) return false;
 
     switch (this.type) {
       case ExprType.TRUE:
       case ExprType.FALSE:
         return true;
       case ExprType.NEGATION:
-        return (this.params[0].equals(expr.params[0]));
+        return (this.params[0].equals(target.params[0]));
       default:  // CONJUNCTION, DISJUNCTION, IMPLICATION
-        return (this.params[0].equals(expr.params[0])) && (this.params[1].equals(expr.params[1]));
+        return (this.params[0].equals(target.params[0])) && (this.params[1].equals(target.params[1]));
     }
   }
 
   toString(): string {
     const lhs = () => {
-      if (this.params[0].type === ExprType.TRUE || this.params[0].type === ExprType.FALSE) {
+      if (this.params[0].type === ExprType.TRUE || this.params[0].type === ExprType.FALSE || this.params[0].type === ExprType.VARIABLE) {
         return this.params[0].toString();
       }
       return enclose(this.params[0].toString());
     };
 
     const rhs = () => {
-      if (this.params[1].type === ExprType.TRUE || this.params[1].type === ExprType.FALSE) {
+      if (this.params[1].type === ExprType.TRUE || this.params[1].type === ExprType.FALSE || this.params[1].type === ExprType.VARIABLE) {
         return this.params[1].toString();
       }
       return enclose(this.params[1].toString());
@@ -68,9 +91,9 @@ class Expr<T extends ExprType> {
   }
 }
 
-const True = new Expr(ExprType.TRUE);
-const False = new Expr(ExprType.FALSE);
-const X = new Expr(ExprType.CONJUNCTION, True, False);
-const Y = new Expr(ExprType.DISJUNCTION, X, new Expr(ExprType.IMPLICATION, False, new Expr(ExprType.NEGATION, X)));
+const True = new LogicExpr(ExprType.TRUE);
+const False = new LogicExpr(ExprType.FALSE);
+const X = new LogicExpr(ExprType.CONJUNCTION, True, False);
+const Y = new LogicExpr(ExprType.DISJUNCTION, X, new LogicExpr(ExprType.IMPLICATION, False, new LogicExpr(ExprType.NEGATION, X)));
 
 console.log(Y.toString());
